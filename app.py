40b1d988d9ebe6461551292a6f2876ae25063f26
nesteswon <nestes@nate.com>
2025-05-13 09:57:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def validate_art_references(df):
 
     
 def notify_slack_of_xml_error(error_message, filename="(알 수 없음)"):
-    webhook_url = "https://hooks.slack.com/services/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"
+    webhook_url = "https://hooks.slack.com/services/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"
     payload = {
         "text": f"*MEC XML 유효성 검사 실패!*\n📄 파일명: `{filename}`\n```{error_message}```"
     }
